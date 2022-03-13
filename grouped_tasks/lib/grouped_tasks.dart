3ec83e330c1f7bf25b9030a23c2ac70dbc589585
Najib Ghadri <najibg96@gmail.com>
2022-03-13 14:58:05 +0100

    add grouped_tasks sub package

---
+++ grouped_tasks/lib/grouped_tasks.dart
@@ -0,0 +1,1 @@
+library grouped_tasks;
---
+++ grouped_tasks/test/grouped_tasks_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:grouped_tasks/grouped_tasks.dart';
+
+void main() {
+  test('adds one to input values', () {
+    // TODO calculate progressbar
+    // final calculator = Calculator();
+    // expect(calculator.addOne(2), 3);
+    // expect(calculator.addOne(-7), -6);
+    // expect(calculator.addOne(0), 1);
+  });
+}
