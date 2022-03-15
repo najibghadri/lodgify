38d534bc2f2c342b5f041fe8ae9b50287f6ba6a2
Najib Ghadri <najibg96@gmail.com>
2022-03-15 15:23:00 +0100

    theming

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:lodgify/ui/accordion_page.dart';
 import 'package:lodgify/ui/theme.dart';
 
@@ -10,20 +9,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Logdify',
-      theme: ThemeData(
-        primarySwatch: createMaterialColor(LodgifyColors.mint),
-        textTheme: GoogleFonts.sourceSansProTextTheme(const TextTheme(
-            headline4: TextStyle(
-                fontSize: 22.0,
-                fontWeight: FontWeight.bold,
-                color: LodgifyColors.text),
-            bodyText1: TextStyle(fontSize: 18, color: LodgifyColors.text),
-            bodyText2: TextStyle(fontSize: 16, color: Colors.black),
-            caption: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.normal,
-                color: LodgifyColors.caption))), // prod: use local assets
-      ),
+      theme: lodgifyTheme,
       home: const AccordionPage(),
     );
   }
--- lib/ui/theme.dart
+++ lib/ui/theme.dart
@@ -30,78 +30,19 @@ MaterialColor createMaterialColor(Color color) {
   return MaterialColor(color.value, swatch);
 }
 
-class LodgifyConstants {
-  LodgifyConstants._();
-  static const double sidePadding = 30;
-  static const double headerSubtitlePadding = 10;
-  static const double defaultRadius = 24;
-}
-
-class LodgifyTypography {
-  LodgifyTypography._();
-  static const subinput = TextStyle(
-    fontSize: 13,
-    color: Colors.black38,
-  );
-  static const subtitle = TextStyle(
-    fontSize: 16,
-    color: Colors.black38,
-  );
-  static const header2 = TextStyle(
-    fontSize: 24,
-    fontWeight: FontWeight.bold,
-  );
-  static const appbar = TextStyle(
-    fontSize: 20,
-    fontWeight: FontWeight.bold,
-  );
-}
-
-final lodgifyTheme = ThemeData.light().copyWith(
-  colorScheme: const ColorScheme.light(
-    primary: Colors.black38,
-    onSecondary: Colors.white,
-    secondary: LodgifyColors.mint,
-  ),
-  textTheme: GoogleFonts.sourceSansProTextTheme(),
-  scaffoldBackgroundColor: Colors.white,
-  appBarTheme: const AppBarTheme(
-    backgroundColor: Colors.white,
-    foregroundColor: Colors.black,
-    // systemOverlayStyle: lightSystemUiStyle,
-    centerTitle: false,
-    elevation: 1,
-    shadowColor: Colors.black45,
-    iconTheme: IconThemeData(color: Colors.black),
-  ),
-  progressIndicatorTheme:
-      const ProgressIndicatorThemeData(color: LodgifyColors.mint),
-  floatingActionButtonTheme:
-      const FloatingActionButtonThemeData(backgroundColor: LodgifyColors.mint),
-  textSelectionTheme: TextSelectionThemeData(
-    cursorColor: LodgifyColors.mint,
-    selectionColor: LodgifyColors.mint.withAlpha(100),
-    selectionHandleColor: LodgifyColors.mint,
-  ),
-  inputDecorationTheme: const InputDecorationTheme(
-    labelStyle: TextStyle(color: Colors.black54),
-    hintStyle: TextStyle(color: Colors.black38),
-    suffixStyle: TextStyle(color: Colors.black38),
-    helperMaxLines: 2,
-    enabledBorder:
-        UnderlineInputBorder(borderSide: BorderSide(color: Colors.black26)),
-    focusedBorder:
-        UnderlineInputBorder(borderSide: BorderSide(color: Colors.black87)),
-  ),
-  textButtonTheme: TextButtonThemeData(
-    style: TextButton.styleFrom(
-        textStyle: const TextStyle(color: LodgifyColors.mint)),
-  ),
-);
-
-const bottomSheetBorder = RoundedRectangleBorder(
-  borderRadius: BorderRadius.vertical(
-    top: Radius.circular(30),
-    bottom: Radius.zero,
-  ),
+final lodgifyTheme = ThemeData(
+  primarySwatch: createMaterialColor(LodgifyColors.mint),
+  textTheme: GoogleFonts.sourceSansProTextTheme(
+    const TextTheme(
+        headline4: TextStyle(
+            fontSize: 22.0,
+            fontWeight: FontWeight.bold,
+            color: LodgifyColors.text),
+        bodyText1: TextStyle(fontSize: 18, color: LodgifyColors.text),
+        bodyText2: TextStyle(fontSize: 16, color: Colors.black),
+        caption: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.normal,
+            color: LodgifyColors.caption)),
+  ), // TODO: in prod use local assets
 );
